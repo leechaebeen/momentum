8549370fddef81a5d3629d7923607dd45b132669
billie <tech.billie@gmail.com>
2021-02-23 23:46:09 +0900

    create todo.js - add 'delete todo'

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -4,7 +4,46 @@ const todoForm = document.querySelector(".js-todoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+// array 의 모든 아이템을 통해 함수를 실행하고
+// true 인 아이템만 가지고 새로운 array 를 만들고.
+// 이 새로운 array 를 보여주겠지
+
+/*
+function filterFn(toDo)
+{
+    return toDo.id === 1
+}
+*/
+
+let toDos = [];
+
+function deleteToDo(event)
+{
+    // ■ target : 이벤트의 대상 정보 출력
+    // console.log(event.target);
+
+    // ■ console.dir : 자바스크립트 객체의 속성들을 출력
+    // console.dir(event.target);
+
+    // console.log(event.target.parentNode);
+
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    // ■ array.filter() → 주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 반환한다.
+    //                  → forEach() 와 유사하다.
+    // 여기서는 클릭한 아이디와 같지 않은 나머지 toDos 를 모아 새로운 배열을 만드는 것이다.
+    const cleanToDos = toDos.filter(function (toDo)
+    {
+            return toDo.id !== parseInt(li.id);
+    });
+
+    // 새로운 배열을 기존 배열에 넣어주고 저장한다.
+    toDos = cleanToDos;
+    saveToDos();
+
+}
 
 // 사용자로부터 입력받은 todos 를 localStorage에 저장하는 함수
 function saveToDos()
@@ -25,6 +64,7 @@ function paintTodo(text)
     const newId = toDos.length + 1;
 
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
 
     // ■ appendChild() : 자식 요소를 추가함
